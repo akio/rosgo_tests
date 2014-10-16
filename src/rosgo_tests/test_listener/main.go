908f30c4e54cb6b5db026184d1f0f13d46d7b2af
Dave Hershberger <dave.hershberger@sri.com>
2014-10-15 20:48:00 -0700

    Applied gofmt

--- src/rosgo_tests/test_listener/main.go
+++ src/rosgo_tests/test_listener/main.go
@@ -1,21 +1,19 @@
 package main
 
 import (
-    "fmt"
-    "ros"
-    "rosgo_tests"
+	"fmt"
+	"ros"
+	"rosgo_tests"
 )
 
-
 func callback(msg *rosgo_tests.Hello) {
-    fmt.Printf("Received: %s\n", msg.Data)
+	fmt.Printf("Received: %s\n", msg.Data)
 }
 
-
 func main() {
-    node := ros.NewNode("/listener")
-    defer node.Shutdown()
-    node.Logger().SetSeverity(ros.LogLevelDebug)
-    node.NewSubscriber("/chatter", rosgo_tests.MsgHello, callback)
-    node.Spin()
+	node := ros.NewNode("/listener")
+	defer node.Shutdown()
+	node.Logger().SetSeverity(ros.LogLevelDebug)
+	node.NewSubscriber("/chatter", rosgo_tests.MsgHello, callback)
+	node.Spin()
 }
--- src/rosgo_tests/test_param/main.go
+++ src/rosgo_tests/test_param/main.go
@@ -1,63 +1,61 @@
 package main
 
 import (
-    "ros"
-    "log"
+	"log"
+	"ros"
 )
 
 func main() {
-    node := ros.NewNode("/test_param")
-    defer node.Shutdown()
+	node := ros.NewNode("/test_param")
+	defer node.Shutdown()
 
-    if hasParam, err := node.HasParam("/rosdistro"); err != nil {
-        log.Fatal(err)
-    } else {
-        if !hasParam {
-            log.Fatal("HasParam() failed.")
-        }
-    }
+	if hasParam, err := node.HasParam("/rosdistro"); err != nil {
+		log.Fatal(err)
+	} else {
+		if !hasParam {
+			log.Fatal("HasParam() failed.")
+		}
+	}
 
+	if foundKey, err := node.SearchParam("/rosdistro"); err != nil {
+		log.Fatal(err)
+	} else {
+		if foundKey != "/rosdistro" {
+			log.Fatal("SearchParam() failed.")
+		}
+	}
 
-    if foundKey, err := node.SearchParam("/rosdistro"); err != nil {
-        log.Fatal(err)
-    } else {
-        if foundKey != "/rosdistro" {
-            log.Fatal("SearchParam() failed.")
-        }
-    }
+	if param, err := node.GetParam("/rosdistro"); err != nil {
+		log.Fatal(err)
+	} else {
+		if value, ok := param.(string); !ok {
+			log.Fatal("GetParam() failed.")
+		} else {
+			if value != "groovy\n" {
+				log.Fatalf("Expected 'groovy\\n' but '%s'", value)
+			}
+		}
+	}
 
-    if param, err := node.GetParam("/rosdistro"); err != nil {
-        log.Fatal(err)
-    } else {
-        if value, ok := param.(string); !ok {
-            log.Fatal("GetParam() failed.")
-        } else {
-            if value != "groovy\n" {
-                log.Fatalf("Expected 'groovy\\n' but '%s'", value)
-            }
-        }
-    }
+	if err := node.SetParam("/test_param", 42); err != nil {
+		log.Fatal(err)
+	}
 
-    if err := node.SetParam("/test_param", 42); err != nil {
-        log.Fatal(err)
-    }
+	if param, err := node.GetParam("/test_param"); err != nil {
+		log.Fatal(err)
+	} else {
+		if value, ok := param.(int32); ok {
+			if value != 42 {
+				log.Fatalf("Expected 42 but %d", value)
+			}
+		} else {
+			log.Fatal("GetParam('/test_param') failed.")
+		}
+	}
 
-    if param, err := node.GetParam("/test_param"); err != nil {
-        log.Fatal(err)
-    } else {
-        if value, ok := param.(int32); ok {
-            if value != 42 {
-                log.Fatalf("Expected 42 but %d", value)
-            }
-        } else {
-            log.Fatal("GetParam('/test_param') failed.")
-        }
-    }
+	if err := node.DeleteParam("/test_param"); err != nil {
+		log.Fatal(err)
+	}
 
-    if err := node.DeleteParam("/test_param"); err != nil {
-        log.Fatal(err)
-    }
-    
-    log.Print("Success")
+	log.Print("Success")
 }
-
